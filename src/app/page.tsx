8d4244e11e9a7e0580713f0a313cfed14a640583
Mikkel Duch <rexboard.dev@gmail.com>
2024-08-30 14:32:11 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ const LandingPage = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <Grid />
         <RecentProjects />
         <Experience />
         <Approach />
