dc20f260d5056608d9d1679bf3037127de1318f6
Mikkel Duch <rexboard.dev@gmail.com>
2024-09-02 14:40:21 +0200

    initial commit

--- src/components/ui/BentoGrid.tsx
+++ src/components/ui/BentoGrid.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -69,18 +70,23 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
+              priority
               src={img}
               alt={img}
+              width={688}
+              height={569}
               className={cn(imgClassName, "object-cover object-center")}
             />
           )}
         </div>
         <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}>
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
+              width={0}
+              height={0}
               className={"object-cover object-center"}
             />
           )}
