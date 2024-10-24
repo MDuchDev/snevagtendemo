732a689183660d4b5a1dedf703a02dc65cd989a6
Mikkel Duch <rexboard.dev@gmail.com>
2024-10-24 17:14:41 +0200

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,56 @@ const LandingPage = () => {
               chatflowid: "964318ce-81c9-45ca-b30e-9100c59aa404",
               apiHost: "https://flowise-v89v.onrender.com",
               theme: {
-                disclaimer: {
-                  title: 'Disclaimer',
-                  message: 'By using this chatbot, you agree to the <a target="_blank" href="https://flowiseai.com/terms">Terms & Condition</a>',
+                button: {
+                  backgroundColor: "#132b40",
+                  size: "large",
+                  right: 50,
+                  bottom: 30,
+                },
+                tooltip: {
+                  tooltipBackgroundColor: "#ffffff",
+                  tooltipTextColor: "black",
+                  tooltipFontSize: 14,
                 },
                 chatWindow: {
-                  titleAvatarSrc: 'https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg',
+                  height: 640,
+                  width: 440,
+                  showTitle: true,
+                  title: 'Snevagten chatbot',
+                  welcomeMessage: "Hej og velkommen til Snevagten.dk. Som AI-assistent er jeg her udelukkende for at hjælpe dig, så spørg bare løs.",
+                  errorMessage: "Der skete en fejl i vores ende, det beklager vi meget.",
+                  botMessage: {
+                    showAvatar: true,
+                    avatarSrc: "/sne-logo.jpg",
+                  },
+                  titleAvatarSrc: '/sne-logo.jpg',
                   feedback: {
                     color: "#303235",
                   },
+                  userMessage: {
+                    showAvatar: true,
+                    backgroundColor: "#3B81F6",
+                    avatarSrc: "/user-logo3.jpg",
+                  },
+                  textInput: {
+                    placeholder: "Skriv besked",
+                    sendButtonColor: '#4d748c',
+                    sendMessageSound: true,
+                    receiveMessageSound: true,
+                    maxChars: 300,
+                    maxCharsWarningMessage: "Maksimum 300 tegn.",
+                    autoFocus: true,
+                  },
+                  footer: {
+                    textColor: '#4d748c',
+                    text: 'Powered by',
+                    company: 'Rexai',
+                    companyLink: 'https://rexai.dk',
+                  }
+                },
+                disclaimer: {
+                  title: 'Disclaimer',
+                  message: 'By using this chatbot, you agree to the <a target="_blank" href="https://flowiseai.com/terms">Terms & Condition</a>',
                 },
               }
             });
